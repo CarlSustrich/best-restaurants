a34f74f67c2461ad2dc6f69441632f5e8dd4a50c
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-03-08 09:18:06 -0800

    initialize project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Best_Restaurants.Solution.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
