a6712ab71768a1eab472f8c23ea01ec7e775d40b
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-03-09 09:22:46 -0800

    add other files

--- BestRestaurant/Controllers/CuisinesControllers.cs
+++ BestRestaurant/Controllers/CuisinesControllers.cs
@@ -40,5 +40,29 @@ public class CuisinesController : Controller
     return View(targetCuisine);
   }
 
-
+  public ActionResult Edit(int id)
+  {
+    Cuisine thisCuisine = _db.Cuisines.FirstOrDefault(cuisine => cuisine.CuisineId == id);
+    return View(thisCuisine);
+  }
+  [HttpPost]
+  public ActionResult Edit(Cuisine cuisine)
+  {
+    _db.Cuisines.Update(cuisine);
+    _db.SaveChanges();
+    return RedirectToAction("Index");
+  }
+  public ActionResult Delete(int id)
+  {
+    Cuisine thisCuisine = _db.Cuisines.FirstOrDefault(cuisine => cuisine.CuisineId == id);
+    return View(thisCuisine);
+  }
+  [HttpPost, ActionName("Delete")]
+  public ActionResult DeleteConfirmed(int id)
+  {
+    Cuisine thisCuisine = _db.Cuisines.FirstOrDefault(cuisine => cuisine.CuisineId == id);
+    _db.Cuisines.Remove(thisCuisine);
+    _db.SaveChanges();
+    return RedirectToAction("Index");
+  }
 }
--- BestRestaurant/Controllers/RestaurantsController.cs
+++ BestRestaurant/Controllers/RestaurantsController.cs
@@ -3,6 +3,7 @@ using BestRestaurant.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BestRestaurant.Controllers
 {
@@ -15,30 +16,58 @@ namespace BestRestaurant.Controllers
       _db = db;
     }
 
-    // public ActionResult Index()
-    // {
-    //   List<Restaurant> model = _db.Restaurants.ToList();
-      
-    //   return View(model);
-    // }
+    public ActionResult Index()
+    {
+      List<Restaurant> model = _db.Restaurants.Include(restaurant => restaurant.Cuisine)
+      .ToList();
+      return View(model);
+    }
 
-    // public ActionResult Create()
-    // {
-    //   return View();
-    // }
+    public ActionResult Create()
+    {
+      ViewBag.CuisineId = new SelectList(_db.Cuisines, "CuisineId", "CuisineType");
+      return View();
+    }
 
-    // [HttpPost]
-    // public ActionResult Create(Animal animal)
-    // {
-    //   _db.Animals.Add(animal);
-    //   _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
+    [HttpPost]
+    public ActionResult Create(Restaurant restaurant)
+    {
+      _db.Restaurants.Add(restaurant);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
-    // public ActionResult Details(int id)
-    // {
-    //   Animal thisAnimal = _db.Animals.FirstOrDefault(animal => animal.AnimalId == id);
-    //   return View(thisAnimal);
-    // }
-}
+    public ActionResult Details(int id)
+    {
+      Restaurant thisRestaurant = _db.Restaurants.Include(restaurant => restaurant.Cuisine)
+      .FirstOrDefault(fish => fish.RestaurantId == id);
+      return View(thisRestaurant);
+    }
+    public ActionResult Edit(int id)
+    {
+      Restaurant thisRestaurant = _db.Restaurants.FirstOrDefault(restaurant => restaurant.RestaurantId == id);
+      ViewBag.CuisineId = new SelectList(_db.Cuisines, "CuisineId", "CuisineType");
+      return View(thisRestaurant);
+    }
+    [HttpPost]
+    public ActionResult Edit(Restaurant restaurant)
+    {
+      _db.Restaurants.Update(restaurant);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult Delete(int id)
+    {
+      Restaurant thisRestaurant = _db.Restaurants.FirstOrDefault(restaurant => restaurant.RestaurantId == id);
+      return View(thisRestaurant);
+    }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Restaurant thisRestaurant = _db.Restaurants.FirstOrDefault(restaurant => restaurant.RestaurantId == id);
+      _db.Restaurants.Remove(thisRestaurant);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
 }
